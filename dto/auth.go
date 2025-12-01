a56b90f399df5f1b8245a9b9091c52446e5acb00
naufalnurhidayat <naufal.gen13@gmail.com>
2025-12-01 16:37:12 +0700

    feat(auth): include user info in signin response and return user from Authenticate

--- dto/auth.go
+++ dto/auth.go
@@ -8,3 +8,24 @@ type SignInRequest struct {
 type SignInResponse struct {
 	Token string `json:"token"`
 }
+
+type SignInUser struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type SignInResult struct {
+	User  SignInUser `json:"user"`
+	Token string     `json:"token"`
+}
+
+type SignInData struct {
+	Result SignInResult `json:"result"`
+}
+
+type SignInFullResponse struct {
+	Code    int        `json:"code"`
+	Message string     `json:"message"`
+	Data    SignInData `json:"data"`
+}
